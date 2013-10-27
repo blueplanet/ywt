70be60a1207b8d2b1e20ccef040ac18062d27084
blueplanet <erguolinge@gmail.com>
2013-10-27 21:26:10 +0900

    YwtItem削除処理を追加

--- app/controllers/ywt_items_controller.rb
+++ app/controllers/ywt_items_controller.rb
@@ -1,13 +1,25 @@
 class YwtItemsController < ApplicationController
+  before_action :set_page, only: [:create, :destroy]
+
   def create
-    @page = Page.where(token: params[:page_token]).first
     @ywt_item = @page.ywts.build ywt_item_params
     @ywt_item.save
 
-    redirect_to page_path(@page, token: @page.token)
+    redirect_to page_path(@page.token)
+  end
+
+  def destroy
+    @ywt_item = YwtItem.find params[:id]
+    @ywt_item.destroy
+
+    redirect_to page_path(@page.token)
   end
 
   private
+    def set_page
+      @page = Page.where(token: params[:page_token]).first
+    end
+
     def ywt_item_params
       params.require(:ywt_item).permit(:body, :item_type)
     end
